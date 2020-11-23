06fe9fb65aeb479f17e62b1c6dd763206954720c
hyurai <yuumanagai0504@icloud.com>
2020-11-23 07:18:28 +0000

    route.rb_complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'infomations#index'
+  resources :infomations do
+    resources :comments,only:[:show,:create,:edit,:update,:destroy] do
+      resources :answers,only:[:create]
+    end
+  end
+  resources :users
+  resources :infomations do
+    resources :favorites,only:[:create,:destroy]
+  end
+  resources :photos,only:[:new,:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
