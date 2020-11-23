f97ae0cb8fdf623e237e581dabf25f87ece80c5f
hyurai <yuumanagai0504@icloud.com>
2020-11-23 07:05:20 +0000

    create_comment_controller_modle_migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_065736) do
+ActiveRecord::Schema.define(version: 2020_11_23_070341) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "infomation_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "infomations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "infomation_id"
