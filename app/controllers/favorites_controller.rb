8373fef7bbbc59fe28e55d535913877823390217
hyurai <yuumanagai0504@icloud.com>
2020-12-14 05:35:06 +0000

    favorite_controller_createmethod_+redirect_to root_path

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,7 @@ class FavoritesController < ApplicationController
     def create
          @favorite = Favorite.create(infomation_id: create_params[:infomation_id],user_id: current_user.id)
          Favorite.order("count(infomation_id) desc")
+         redirect_to root_path
     end
     def destroy
         @favorite = Favorite.find_by(infomation_id: params[:infomation_id],user_id: current_user.id)
